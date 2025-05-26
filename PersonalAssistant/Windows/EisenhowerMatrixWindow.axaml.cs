0fe5bde2afde3b088c8fdc36c216d40586904545
X0fei <pitonvano05@gmail.com>
2025-05-26 15:38:55 +0300

    Изменена вёрстка окна с матрицей Эйзенхауэра

--- PersonalAssistant/Windows/EisenhowerMatrixWindow.axaml.cs
+++ PersonalAssistant/Windows/EisenhowerMatrixWindow.axaml.cs
@@ -3,6 +3,9 @@ using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using PersonalAssistant.Helpers;
 using PersonalAssistant.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 
 namespace PersonalAssistant;
@@ -10,6 +13,9 @@ namespace PersonalAssistant;
 public partial class EisenhowerMatrixWindow : Window
 {
     private int userID;
+    private List<Models.Task> displayAllTasks = DBContext.Tasks;
+    // Коллекция задач на выбранную дату
+    private ObservableCollection<Models.Task> _tasksOnSelectedDate = new ObservableCollection<Models.Task>();
 
     public EisenhowerMatrixWindow()
     {
@@ -41,8 +47,25 @@ public partial class EisenhowerMatrixWindow : Window
 
     private void GoBackButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
     {
-        TasksWindow tasksWindow = new(userID);
-        tasksWindow.Show();
+        if (DBContext.PreviousWindowType != null)
+        {
+            var previousWindow = (Window)Activator.CreateInstance(DBContext.PreviousWindowType, userID);
+            previousWindow.Show();
+            Close();
+        }
+    }
+    private void MoodButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        DBContext.PreviousWindowType = typeof(TasksWindow);
+        Mood moodWindow = new Mood(userID);
+        moodWindow.Show();
+        Close();
+    }
+    private void AddTaskButton_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        DBContext.PreviousWindowType = typeof(TasksWindow);
+        AddEditTask addEditTask = new AddEditTask(userID);
+        addEditTask.Show();
         Close();
     }
 
@@ -55,5 +78,27 @@ public partial class EisenhowerMatrixWindow : Window
             addEditTask.Show();
             Close();
         }
+    }
+    private void TasksCalendar_SelectedDatesChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        Calendar calendar = sender as Calendar;
+        var date = calendar.SelectedDate;
+        if (date != null)
+        {
+            var tasks = displayAllTasks.Where(t =>
+                t.Users.Any(u => u.Id == userID) &&
+                ((t.StartDate?.Date <= date && t.EndDate?.Date >= date) ||
+                (t.StartDate?.Date == null && t.EndDate?.Date == date) ||
+                (t.StartDate?.Date == date && t.EndDate?.Date == null) ||
+                (t.Deadline?.Date == date))).ToList();
+
+            _tasksOnSelectedDate.Clear();
+            foreach (var task in tasks)
+                _tasksOnSelectedDate.Add(task);
+
+            TasksByDateList.ItemsSource = _tasksOnSelectedDate;
+        }
+        CurrentDateTextBlock.Text = char.ToUpper((char)(date?.Date.ToString("dddd", new System.Globalization.CultureInfo("ru-RU"))[0])) +
+                   date?.Date.ToString("dddd, d MMMM", new System.Globalization.CultureInfo("ru-RU")).Substring(1);
     }
 }
